4805853ddacf5037570585eebeb98e713c16069b
Андрей Шкарупа <80199664+and-rey-sh02@users.noreply.github.com>
2021-06-06 12:53:03 +0300

    Create Variables.cs
    C_Sharp

---
+++ Variables/Variables.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Variables
+    {
+        static void Main(string[] args)
+        {
+         int x = 42;
+         double pi = 3.14;
+         char y = 'Z';
+         bool isOnline = true;
+         string firstName = "David";
+         Console.WriteLine(x);
+         Console.WriteLine(pi);
+         Console.WriteLine(y);
+         Console.WriteLine(isOnline);
+         Console.WriteLine(firstName);
+        }
+    }
+}
