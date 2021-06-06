d22cec37009cf850db8bd519bfe9851395d6bf8d
Андрей Шкарупа <80199664+and-rey-sh02@users.noreply.github.com>
2021-06-06 12:15:02 +0300

    Create HelloWorld.cs
    C_Sharp

---
+++ HelloWorld.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_CSharp_Program
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!");
+        }
+    }
+}
