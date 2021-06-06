bbc7bec169196a0fd7b89797e87f6a64c3bd0e4e
Андрей Шкарупа <80199664+and-rey-sh02@users.noreply.github.com>
2021-06-07 01:21:22 +0300

    Create Constants.cs
    C_Sharp

---
+++ Constants/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Constants
+{
+    class Constants
+    {
+        static void Main(string[] args)
+        {
+         const float x = 5.90f;
+         const uint y = 10;
+         Console.WriteLine(x);
+         Console.WriteLine(y);
+        }
+    }
+}
