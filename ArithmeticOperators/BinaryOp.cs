554d4208936b239898a9f66c26a87a64d7620c32
Андрей Шкарупа <80199664+and-rey-sh02@users.noreply.github.com>
2021-06-07 22:18:06 +0300

    Create BinaryOp.cs
    C_Sharp

---
+++ ArithmeticOperators/BinaryOp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArithmeticOp
+{
+    class BinaryOp
+    {
+        static void Main(string[] args)
+        {
+         int x = 10, y = 5;
+         Console.WriteLine(x + y); // 15
+         Console.WriteLine(x - y); // 5
+         Console.WriteLine(x * y); // 50
+         Console.WriteLine(x / y); // 2
+         Console.WriteLine(x % y); // 0
+        }
+    }
+}
