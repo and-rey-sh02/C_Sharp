3979a27bb7171fe4f069b20c8813f319770da1be
Андрей Шкарупа <80199664+and-rey-sh02@users.noreply.github.com>
2021-06-13 00:02:15 +0300

    Create Array.cs
    C_Sharp

---
+++ Arrays/Array.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         int[] mass = new int[10];
+         Random rand = new Random();
+          for (int i = 0; i < mass.Length; i++)
+           {
+            mass[i] = rand.Next(10);
+            Console.WriteLine("{0} \n", mass[i]);
+           }
+        }
+    }
+}
