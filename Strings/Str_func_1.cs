34d9b4fba7d940a5923197c8fc87a19848c3d027
Андрей Шкарупа <80199664+and-rey-sh02@users.noreply.github.com>
2021-06-26 12:10:20 +0300

    Create Str_func_1.cs
    C_Sharp

---
+++ Strings/Str_func_1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Str_func_1
+{
+    class Str_func_1
+    {
+        static void Main(string[] args)
+        {
+         string str_1 = "Hello";
+         string str_2 = "";
+         string str_3 = "\t";
+         string str_4 = "HellO WorLD";
+         string str_5 = "C, C++, C#";
+         char[] sym = str_4.ToCharArray();
+         string[] prog = str_5.Split(',');
+         Console.WriteLine(sym[4]); // O
+         Console.WriteLine(prog[1]); // C++
+         Console.WriteLine(str_1[1]); // e
+         Console.WriteLine(str_1.Length); // 5
+         Console.WriteLine(String.IsNullOrEmpty(str_1)); // False
+         Console.WriteLine(String.IsNullOrEmpty(str_2)); // True
+         Console.WriteLine(String.IsNullOrWhiteSpace(str_3)); // True
+         Console.WriteLine(String.Compare("a","b")); // -1
+         Console.WriteLine(String.Compare("b", "a")); // 1
+         Console.WriteLine(String.Compare("a", "a")); // 0
+         Console.WriteLine(str_4.ToLower()); // hello world
+         Console.WriteLine(str_4.ToUpper()); // HELLO WORLD
+         Console.WriteLine(str_4.Contains("llO")); // True
+         Console.WriteLine(str_4.StartsWith("Hell")); // True
+         Console.WriteLine(str_4.EndsWith("LD")); // True
+         Console.WriteLine(str_4.Insert(5,"LD")); // HellOLD WorLD
+         Console.WriteLine(str_4.Remove(5)); // HellO
+         Console.WriteLine(str_4.Replace("llO","llo")); // Hello WorLD
+
+        }
+    }
+}
