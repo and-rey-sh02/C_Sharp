fbd86d072421910479e1cfb933be23abc35b0d02
Андрей Шкарупа <80199664+and-rey-sh02@users.noreply.github.com>
2021-06-27 13:00:27 +0300

    Create try-catch.cs
    C_Sharp

---
+++ Operators/try-catch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace try_catch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number: ");
+            try
+            {
+                int num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Num is " + num);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Format Exception");
+            }
+            finally
+            {
+                Console.WriteLine("Program completed");
+            }
+        }
+    }
+}
