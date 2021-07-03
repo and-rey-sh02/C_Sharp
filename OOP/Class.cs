2265baf27fd39a3ca59197ba49baa26e2ff1f26d
Андрей Шкарупа <80199664+and-rey-sh02@users.noreply.github.com>
2021-07-03 13:31:49 +0300

    Create Class.cs
    C_Sharp

---
+++ OOP/Class.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    class Animal
+    {
+        public string name = "Spotty";
+        public int age = 7;
+        public float hapiness = 0.6f;
+        public void Print()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("hapiness " + hapiness);
+        }
+    }
+    class Class
+    {
+        static void Main(string[] args)
+        {
+            Animal cat = new Animal();
+            cat.Print();
+        }
+    }
+}
