8002b571e881eb4cbc9ff28d2b9d830d81ec0011
Андрей Шкарупа <80199664+and-rey-sh02@users.noreply.github.com>
2021-07-04 12:48:13 +0300

    Create Recursion.cs
    C_Sharp

---
+++ Methods/Recursion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    class Recursion
+    {
+        static int Fact(int num)
+        {
+            if(num == 1)
+            {
+                return 1;
+            }
+            return num * Fact(num - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fact(6));
+        }
+    }
+}
