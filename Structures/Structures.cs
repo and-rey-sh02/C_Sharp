c8f98481a279663a04efb9d65625db7d1d007d52
Андрей Шкарупа <80199664+and-rey-sh02@users.noreply.github.com>
2021-07-12 11:41:46 +0300

    Create Structures.cs
    C_Sharp

---
+++ Structures/Structures.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structures
+{
+    struct Book
+    {
+        public string title;
+        public string name;
+        public string author;
+    }
+    class Structures
+    {
+        static void Main(string[] args)
+        {
+            Book b;
+            b.title = "C#";
+            b.name = "Andrey";
+            b.author = "King";
+            Console.WriteLine(b.title);
+            Console.WriteLine(b.name);
+            Console.WriteLine(b.author);
+        }
+    }
+}
