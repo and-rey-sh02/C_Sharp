c6a5226c32275ca2597ffd494db8d873e2e9ad5f
Андрей Шкарупа <80199664+and-rey-sh02@users.noreply.github.com>
2021-07-12 21:05:42 +0300

    Create Sum_method.cs
    C_Sharp

---
+++ Arrays/Array_methods/Sum_method.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_method
+{
+    class Sum_method
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 4, 7, 1 };
+            Console.WriteLine(arr.Sum()); //14
+        }
+    }
+}
