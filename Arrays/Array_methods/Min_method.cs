353046fa468828053aeefe2b4323bb8edae50cf8
Андрей Шкарупа <80199664+and-rey-sh02@users.noreply.github.com>
2021-07-12 21:05:34 +0300

    Create Min_method.cs
    C_Sharp

---
+++ Arrays/Array_methods/Min_method.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Min_method
+{
+    class Min_method
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 4, 7, 1 };
+            Console.WriteLine(arr.Min()); //1
+        }
+    }
+}
