bc6761725033ee506e1e5603a6cbd83e640a9174
Андрей Шкарупа <80199664+and-rey-sh02@users.noreply.github.com>
2021-07-17 11:11:14 +0300

    Create Nested_Classes.cs
    C_Sharp

---
+++ OOP/Nested_Classes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nested_Classes
+{
+    class Nested_Classes
+    {
+        class Car
+        {
+            string name;
+            public Car(string _name)
+            {
+                name = _name;
+                Console.WriteLine("Class Car");
+                Motor m = new Motor(5);
+            }
+            public class Motor
+            {
+                int volume;
+                public Motor(int _volume)
+                {
+                    volume = _volume;
+                    Console.WriteLine("Class Motor");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Car c = new Car("Class Car");
+        }
+    }
+}
