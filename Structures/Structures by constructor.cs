06394f69e40eaba0696ade0784fa63c3fa6013ff
Андрей Шкарупа <80199664+and-rey-sh02@users.noreply.github.com>
2021-07-17 11:29:17 +0300

    Create Structures by constructor.cs
    C_Sharp

---
+++ Structures/Structures by constructor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structures_by_constructor
+{
+    class Program
+    {
+        struct Point
+        {
+            public int x;
+            public int y;
+            public Point(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Point p = new Point(10, 15);
+            Console.WriteLine(p.x);
+            Console.WriteLine(p.y);
+        }
+    }
+}
