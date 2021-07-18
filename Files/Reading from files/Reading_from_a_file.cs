8f3bec7657ee827e99750905d6543e8bc3eb37ba
Андрей Шкарупа <80199664+and-rey-sh02@users.noreply.github.com>
2021-07-18 13:15:33 +0300

    Create Reading_from_a_file.cs
    C_Sharp

---
+++ Files/Reading from files/Reading_from_a_file.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Reading_from_a_file
+{
+    class Reading_from_a_file
+    {
+        static void Main(string[] args)
+        {
+            string str = "Some text";
+            File.WriteAllText("test.txt", str);
+            string txt = File.ReadAllText("test.txt");
+            Console.WriteLine(txt);
+        }
+    }
+}
