4e4c3bc67f4f511c29af8543785ba99722490558
Андрей Шкарупа <80199664+and-rey-sh02@users.noreply.github.com>
2021-07-18 22:33:06 +0300

    Create Cycle_foreach.cs
    C_Sharp

---
+++ Cycle/foreach loops/Cycle_foreach.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         int[] mass = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+          foreach(int el in mass)
+          {
+           Console.WriteLine(el);
+          }
+        }
+    }
+}
