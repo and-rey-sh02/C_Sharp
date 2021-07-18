19509b14be35f01838ef556c0b1a89b5f9cbf854
Андрей Шкарупа <80199664+and-rey-sh02@users.noreply.github.com>
2021-07-18 12:31:48 +0300

    Create Max_method.cs
    C_Sharp

---
+++ Arrays/Array methods/Max_method.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Max_method
+{
+    class Max_method
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 4, 7, 1 };
+            Console.WriteLine(arr.Max()); //7
+        }
+    }
+}
