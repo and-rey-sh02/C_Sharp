600fe13082d590390dcc3f67b1af5fda0278b076
Андрей Шкарупа <80199664+and-rey-sh02@users.noreply.github.com>
2021-07-18 12:32:54 +0300

    Create Stepped_arrays.cs
    C_Sharp

---
+++ Arrays/Stepped arrays/Stepped_arrays.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stepped_arrays
+{
+    class Stepped_arrays
+    {
+        static void Main(string[] args)
+        {
+            int[ ][ ] jaggedArr = new int[ ][ ]
+            {
+                new int[ ] { 1, 8, 2, 7, 9 },
+                new int[ ] { 2, 4, 6 },
+                new int[ ] { 33, 42 }
+            };
+            int x = jaggedArr[2][1];
+            Console.WriteLine(x); //42
+        }
+    }
+}
