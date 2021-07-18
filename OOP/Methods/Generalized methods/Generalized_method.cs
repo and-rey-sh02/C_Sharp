de5ac28695e5ad3c0cdbbb56f3265f19067644af
Андрей Шкарупа <80199664+and-rey-sh02@users.noreply.github.com>
2021-07-18 22:59:20 +0300

    Create Generalized_method.cs
    C_Sharp

---
+++ OOP/Methods/Generalized methods/Generalized_method.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generalized_method
+{
+    class Generalized_method
+    {
+        static void Swap<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+        static void Main(string[] args)
+        {
+            int a = 4, b = 9;
+            Swap<int>(ref a, ref b);
+            Console.WriteLine(a+" "+b);
+
+            string x = "Hello";
+            string y = "Word";
+            Swap<string>(ref x, ref y);
+            Console.WriteLine(x+" "+y);
+        }
+    }
+}
