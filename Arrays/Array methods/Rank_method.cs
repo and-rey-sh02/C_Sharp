b00718a10dc467386fd1aa319d841bb07a36b6fb
Андрей Шкарупа <80199664+and-rey-sh02@users.noreply.github.com>
2021-07-18 12:32:04 +0300

    Create Rank_method.cs
    C_Sharp

---
+++ Arrays/Array methods/Rank_method.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rank_method
+{
+    class Rank_method
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 4, 7 };
+            Console.WriteLine(arr.Rank); //1
+        }
+    }
+}
