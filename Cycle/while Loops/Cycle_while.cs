b1ec4f85a0323819f595297d7f1823dc7f1fc15f
Андрей Шкарупа <80199664+and-rey-sh02@users.noreply.github.com>
2021-07-18 22:33:15 +0300

    Create Cycle_while.cs
    C_Sharp

---
+++ Cycle/while Loops/Cycle_while.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While
+{
+    class Cycle_While
+    {
+        static void Main(string[] args)
+        {
+         int i = 0;
+          while(i < 10)
+           {
+            Console.WriteLine($"{i} \n");
+            i++;
+           }
+        }
+    }
+}
