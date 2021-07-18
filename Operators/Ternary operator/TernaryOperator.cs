114d4a14cb434dc9c9f339974b42e58f6adeb03a
Андрей Шкарупа <80199664+and-rey-sh02@users.noreply.github.com>
2021-07-18 12:41:51 +0300

    Create TernaryOperator.cs
    C_Sharp

---
+++ Operators/Ternary operator/TernaryOperator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_else
+{
+    class If_else
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            Console.WriteLine("Enter two numbers: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            string c = a == b
+             ? "The numbers are equal"
+             : "The numbers are not equal";
+            Console.WriteLine(c);
+        }
+    }
+}
