77ee9633256094ec5586c0221d0965e94be12197
Андрей Шкарупа <80199664+and-rey-sh02@users.noreply.github.com>
2021-07-18 13:15:25 +0300

    Create Write_to_file.cs
    C_Sharp

---
+++ Files/Writing to files/Write_to_file.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Write_to_file
+{
+    class Write_to_file
+    {
+        static void Main(string[] args)
+        {
+            string str = "Some text";
+            File.WriteAllText("test.txt", str);
+        }
+    }
+}
