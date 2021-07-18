f754a09a3999c8ae2b5e021e7330d21698d05ce7
Андрей Шкарупа <80199664+and-rey-sh02@users.noreply.github.com>
2021-07-18 12:57:07 +0300

    Create Exception.cs
    C_Sharp

---
+++ Operators/Operator try-catch/Exception.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] arr = new int[] { 4, 5, 8 };
+                Console.WriteLine(arr[8]);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
