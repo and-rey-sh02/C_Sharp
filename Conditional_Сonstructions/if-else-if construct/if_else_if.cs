b2aa0ec5c36f1ec162d4942a85e4bb48f8862fa2
Андрей Шкарупа <80199664+and-rey-sh02@users.noreply.github.com>
2021-07-18 22:26:02 +0300

    Create if_else_if.cs
    C_Sharp

---
+++ Conditional_Сonstructions/if-else-if construct/if_else_if.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_else_if
+{
+    class If_else_if
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            Console.WriteLine("Enter number: ");
+            a = Convert.ToInt32(Console.ReadLine());    
+            if (a <= 50)
+             {
+              Console.WriteLine("{0} <= 50", a);  
+             }
+            else if (a < 99)
+             {
+              Console.WriteLine("{0} < 99", a);
+             }
+            else if (a == 100)
+             {
+              Console.WriteLine("{0} = 100", a);
+             }
+            else
+            {
+             Console.WriteLine("Not in range");
+            }
+        }
+    }
+}
