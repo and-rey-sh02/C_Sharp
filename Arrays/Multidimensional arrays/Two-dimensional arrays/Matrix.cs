a31f9853b0b4f2acbfe5a580d138a947d0288bec
Андрей Шкарупа <80199664+and-rey-sh02@users.noreply.github.com>
2021-07-18 12:32:37 +0300

    Create Matrix.cs
    C_Sharp

---
+++ Arrays/Multidimensional arrays/Two-dimensional arrays/Matrix.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    class Matrix
+    {
+        static void Main(string[] args)
+        {
+         Random rnd = new Random();
+         int[,] arr = new int[5, 5];
+          for(int i = 0; i < 5; i++)
+          {
+           for(int j = 0; j < 5; j++)
+           {
+            arr[i, j] = rnd.Next(0, 10);
+            Console.Write(arr[i, j]+"\t");
+           }
+           Console.WriteLine("");
+          }
+        }
+    }
+}
