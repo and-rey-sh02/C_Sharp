058b822d833aa777e6f6dc3f27ca3c3355daea06
Андрей Шкарупа <80199664+and-rey-sh02@users.noreply.github.com>
2021-07-19 22:39:58 +0300

    Create Queue.cs
    C_Sharp

---
+++ OOP/Collections/Generalized collections/Queue.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    class Queue
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> q = new Queue<int>();
+            q.Enqueue(5);
+            q.Enqueue(10);
+            q.Enqueue(15);
+            Console.Write("Queue: ");
+            foreach (int i in q)
+                Console.Write(i + " ");
+            Console.Write("\nCount: " + q.Count);
+            Console.Write("\nDequeue: " + q.Dequeue());
+            Console.Write("\nQueue:");
+            foreach (int i in q)
+                Console.Write(i + " ");
+            Console.Write("\nCount: " + q.Count);
+        }
+    }
+}
