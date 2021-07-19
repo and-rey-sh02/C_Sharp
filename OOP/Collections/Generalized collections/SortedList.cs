709c76a93d33943a246d8c8b33ee08966807ee93
Андрей Шкарупа <80199664+and-rey-sh02@users.noreply.github.com>
2021-07-19 13:10:13 +0300

    Create SortedList.cs
    C_Sharp

---
+++ OOP/Collections/Generalized collections/SortedList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortedList
+{
+    class SortedList
+    {
+        static void Main(string[] args)
+        {
+            SortedList<string, int> sl = new SortedList<string, int>();
+            sl.Add("Solo", 59);
+            sl.Add("A", 95);
+            sl.Add("Learn", 72);
+            sl.Remove("A");
+            Console.WriteLine("Sorted List: ");
+            foreach (string s in sl.Keys)
+                Console.WriteLine(s + ": " + sl[s]);
+            Console.WriteLine("\n Count: " + sl.Count);
+        }
+    }
+}
