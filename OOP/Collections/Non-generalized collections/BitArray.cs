fba60b8dd9d740d93882e0abe3943557835b6048
Андрей Шкарупа <80199664+and-rey-sh02@users.noreply.github.com>
2021-07-19 13:10:22 +0300

    Create BitArray.cs
    C_Sharp

---
+++ OOP/Collections/Non-generalized collections/BitArray.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bitarray
+{
+    class Program
+    {
+        public static void PrintBarr(string name, BitArray ba)
+        {
+            Console.WriteLine(name + " : ");
+            for (int x = 0; x < ba.Length; x++)
+                Console.WriteLine(ba.Get(x) + " ");
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            BitArray ba1 = new BitArray(4);
+            BitArray ba2 = new BitArray(4);
+
+            ba1.SetAll(true);
+            ba2.SetAll(false);
+
+            ba1.Set(2, false);
+            ba2.Set(3, true);
+
+            PrintBarr("ba1", ba1);
+            PrintBarr("ba2", ba2);
+
+            PrintBarr("ba1 AND ba2", ba1.And(ba2));
+            PrintBarr("Not ba2", ba2.Not());
+        }
+    }
+}
