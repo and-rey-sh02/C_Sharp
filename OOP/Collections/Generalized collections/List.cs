05aedc01e19b8517c09a3867b481adb4f9bdbcdc
Андрей Шкарупа <80199664+and-rey-sh02@users.noreply.github.com>
2021-07-19 13:10:04 +0300

    Create List.cs
    C_Sharp

---
+++ OOP/Collections/Generalized collections/List.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    class List
+    {
+        static void Main(string[] args)
+        {
+            List<int> li = new List<int>();
+            li.Add(59);
+            li.Add(72);
+            li.Add(95);
+            li.Add(5);
+            li.Add(9);
+            li.Remove(1);
+            Console.WriteLine("\n List: ");
+            for (int x = 0; x < li.Count; x++)
+                Console.WriteLine(li[x] + " ");
+            li.Sort();
+            Console.WriteLine("\n Sorted: ");
+            for (int x = 0; x < li.Count; x++)
+                Console.WriteLine(li[x] + " ");
+        }
+    }
+}
