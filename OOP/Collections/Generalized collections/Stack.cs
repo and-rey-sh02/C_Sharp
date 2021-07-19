4c4db8e6e994b875d19c4f3e8b29802e011d429f
Андрей Шкарупа <80199664+and-rey-sh02@users.noreply.github.com>
2021-07-19 22:40:06 +0300

    Create Stack.cs
    C_Sharp

---
+++ OOP/Collections/Generalized collections/Stack.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Stack
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> s = new Stack<int>();
+            s.Push(59);
+            s.Push(72);
+            s.Push(65);
+            Console.Write("Stack: ");
+            foreach (int i in s)
+                Console.Write(i + " ");
+            Console.Write("\nCount: " + s.Count);
+            Console.Write("\nTop: " + s.Peek());
+            Console.Write("\nPop: " + s.Pop());
+            Console.Write("\nStack: ");
+            foreach (int i in s)
+                Console.Write(i + " ");
+            Console.Write("\nCount: " + s.Count);
+        }
+    }
+}
