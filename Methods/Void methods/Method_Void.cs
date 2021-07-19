54e2b96c29c2a808e4de802facf3518823df04a9
Андрей Шкарупа <80199664+and-rey-sh02@users.noreply.github.com>
2021-07-19 11:22:36 +0300

    Create Method_Void.cs
    C_Sharp

---
+++ Methods/Void methods/Method_Void.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Void
+{
+    class Method_Void
+    {
+        static void Add(int num_1, int num_2)
+        {
+         Console.WriteLine("Result = " + (num_1 + num_2).ToString());
+        }
+        static void Main(string[] args)
+        {
+         Console.WriteLine("Enter two numbers ");
+         int n_1 = Convert.ToInt32(Console.ReadLine());
+         int n_2 = Convert.ToInt32(Console.ReadLine());
+         Add(n_1, n_2);
+        }
+    }
+}
