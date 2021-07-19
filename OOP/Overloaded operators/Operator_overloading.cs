fdfdb368471669ab3211d50d0f7ad68105899f02
Андрей Шкарупа <80199664+and-rey-sh02@users.noreply.github.com>
2021-07-19 11:54:24 +0300

    Create Operator_overloading.cs
    C_Sharp

---
+++ OOP/Overloaded operators/Operator_overloading.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_overloading
+{
+    class Operator_overloading
+    {
+        class Box
+        {
+            public int Height { get; set; }
+            public int Width { get; set; }
+            public Box(int h, int w)
+            {
+                Height = h;
+                Width = w;
+            }
+            public static Box operator + (Box a, Box b)
+            {
+                int h = a.Height + b.Height;
+                int w = a.Width + b.Width;
+                Box res = new Box(h, w);
+                return res;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Box b1 = new Box(14, 3);
+            Box b2 = new Box(5, 7);
+            Box b3 = b1 + b2;
+            Console.WriteLine(b3.Height);
+            Console.WriteLine(b3.Width);
+        }
+    }
+}
