fbdd4d9d9a21ed694bca2c0e2bd794dd1867f263
Андрей Шкарупа <80199664+and-rey-sh02@users.noreply.github.com>
2021-07-20 22:22:56 +0300

    Create Output_Interpolation.cs
    C_Sharp

---
+++ Input_Output/Output/Output_Interpolation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Output_Interpolation
+{
+    class Output_Interpolation
+    {
+        static void Main(string[] args)
+        {
+            int a = 5, b = 10, c = 15;
+            Console.WriteLine($"a = {a}\nb = {b}\nc = {c}");
+        }
+    }
+}
