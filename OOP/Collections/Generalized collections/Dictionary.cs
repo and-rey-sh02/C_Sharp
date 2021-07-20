b9c281d44543f9d617728f7fec47d024c7f252d8
Андрей Шкарупа <80199664+and-rey-sh02@users.noreply.github.com>
2021-07-20 12:09:57 +0300

    Create Dictionary.cs
    C_Sharp

---
+++ OOP/Collections/Generalized collections/Dictionary.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Dictionary
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> d = new Dictionary<string, int>();
+            d.Add("Uno", 1);
+            d.Add("One", 1);
+            d.Add("Dos", 2);
+            d.Add("Deux", 2);
+            d.Remove("One");
+            d.Remove("Dos");
+            Console.WriteLine("Dictionary: ");
+            foreach(string s in d.Keys)
+                Console.WriteLine(s + ": " + d[s]);
+            Console.WriteLine("\nCount: {0}", d.Count);
+        }
+    }
+}
